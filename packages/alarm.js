50badddddbe8bf32bbba39cfa7e236834509c486
Jiseop Han 한지섭 <jshan2017@gmail.com>
2020-06-20 22:33:00 +0900

    Add package: alarm

---
+++ packages/alarm.js
@@ -0,0 +1,16 @@
+function alarm(){
+  if (args.length !== 3) {
+    const errorMsg = "Error: try again with 'alarm [hh] [mm] [ss]";
+    stream.writeStderr(errorMsg);
+    return;
+  }
+  const hours = parseInt(args[0]);
+  const minutes = parseInt(args[1]);
+  const seconds = parseInt(args[2]);
+  const duration = hours * 60 * 60 + minutes * 60 + seconds;
+  const outputMsg = "Alarm set";
+  stream.writeStdout(outputMsg);
+  setTimeout(() => {
+    alert("🚨 Don't miss me! 🚨");
+  }, 1000 * duration);
+}
