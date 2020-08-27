8178be640284bd734956164dd238264140beaa71
Jiseop Han <jshan2017@gmail.com>
2020-08-27 09:35:41 +0900

    Create now.js

---
+++ now.js
@@ -0,0 +1,13 @@
+  
+function time(){
+  if (args.length !== 0) {
+    const errorMsg = "Error: try again with 'now";
+    stream.writeStderr(errorMsg);
+    return;
+  }
+  const date = new Date();
+  const outputMsg = date.toLocaleString();
+  stream.writeStdout(outputMsg);
+}
+
+time();
